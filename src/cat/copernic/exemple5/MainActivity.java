8ae32eb582b845f9f21c3603ea0f5f0acda192dd
Marc <marc.siso@hotmail.com>
2015-03-23 12:46:54 +0100

    Update MainActivity.java

--- src/cat/copernic/exemple5/MainActivity.java
+++ src/cat/copernic/exemple5/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+//Comentari de prova
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
